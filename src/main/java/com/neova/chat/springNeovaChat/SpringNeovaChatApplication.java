68d702f3989ca1a26477f8bbd33eae6386ecaaba
suraj_bhosale <suraj_bhosale@neovasolutions.com>
2020-03-14 13:15:29 +0530

    first commit

---
+++ src/main/java/com/neova/chat/springNeovaChat/SpringNeovaChatApplication.java
@@ -0,0 +1,13 @@
+package com.neova.chat.springNeovaChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNeovaChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNeovaChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neova/chat/springNeovaChat/SpringNeovaChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neova.chat.springNeovaChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNeovaChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
